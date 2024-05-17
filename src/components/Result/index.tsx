c251b62ce8b401ecc3202c207b06711d780977fe
Diego Chagas <diegofchagas@gmail.com>
2024-05-17 20:13:35 -0300

    table pronta falta estilizações

--- src/components/Result/index.tsx
+++ src/components/Result/index.tsx
@@ -15,18 +15,25 @@ export const Result = () => {
           <td>Salário Bruto</td>
           <td>-</td>
           <td>R$4.000,00</td>
-          <td>R$500,00</td>
+          <td>-</td>
         </tr>
         <tr>
           <td>INSS</td>
           <td>14.00%</td>
+          <td>-</td>
           <td>R$480,00</td>
         </tr>
         <tr>
           <td>IRRF</td>
+          <td>15.00%</td>
+          <td>-</td>
+          <td>285,15</td>
         </tr>
         <tr>
           <td>Total</td>
+          <td>-</td>
+          <td>4000</td>
+          <td>600</td>
         </tr>
       </tbody>
     </table>
