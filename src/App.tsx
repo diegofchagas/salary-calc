22ff24e42c2191bad30f34b4ba4f14112615c7dc
Diego Chagas <diegofchagas@gmail.com>
2024-05-16 21:04:53 -0300

    add form controll de inputs

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { FormControlInputs } from "./components/FormControlInputs";
+
+export const App = () => {
+  return (
+    <>
+      <p>oi</p>
+
+      <FormControlInputs/>
+    </>
+  );
+};
---
+++ src/components/FormControlInputs/index.tsx
@@ -0,0 +1,11 @@
+
+
+export const FormControlInputs  = () => {
+  return (
+    <form>
+      <input type="number" placeholder="Informe seu salário"/>
+      <input type="number" placeholder="Descontos" />
+      <input type="number" placeholder="Dependentes" />
+    </form>
+  )
+}
