90924ee467e9aeb04ba759e06583c9e0dc0b926c
Diego Chagas <diegofchagas@gmail.com>
2024-11-08 15:19:14 -0300

    ajustando error de compilador

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
-import { Results } from "./components/Result";
+
 import { Home } from "./pages/Home";
-import TableTwo from "./pages/Home/table";
+
 
 export const App = () => {
   return (
     <>
     <Home/>
-    {/* <TableTwo results={{
-        grossSalary: 0,
-        inss: 0,
-        irrf: 0,
-        netSalary: 0,
-        discountTotals: 0,
-        aliquotInss: 0,
-        aliquotIrpf: 0,
-        others: undefined
-      }}/> */}
-
-    {/* <Results results={{
-        grossSalary: 0,
-        inss: 0,
-        irrf: 0,
-        netSalary: 0,
-        discountTotals: 0,
-        aliquotInss: 0,
-        aliquotIrpf: 0,
-        others: undefined
-      }}/> */}
     </>
   );
 };
